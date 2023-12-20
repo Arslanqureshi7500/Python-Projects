5259762eb61ea4d71917c2f48db619efc9dc1d8a
Arslanqureshi7500 <154023599+Arslanqureshi7500@users.noreply.github.com>
2023-12-20 05:32:02 +0500

    Functions with Outputs

---
+++ 10 - Functions with Outputs/Lec 103 - Calculator combining Dictionaries and functions.py
@@ -0,0 +1,45 @@
+#Adding
+from art import logo
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def mul(num1, num2):
+    return num1 * num2
+
+def div(num1, num2):
+    return num1 / num2
+
+operations = {
+    "+" : add,
+    "-" : sub,
+    "*" : mul,
+    "/" : div
+    }
+
+
+def calculator():
+    print(logo)
+    num1 = int(input("What's first number? : "))
+    for symbol in operations:
+        print(symbol)    
+    should_continue = True
+
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's next number? : "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' start a new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+            
+calculator()
